2c826605d5ae6d3f3732ef9326e8a108524cf362
Akin Arslan <arslakin@gmail.com>
2026-07-18 07:30:35 -0400

    Add CDK infrastructure for RojAI Always-On Agent

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,7 +1,15 @@
 import * as cdk from "aws-cdk-lib";
 import { RojAIStack } from "../lib/rojAI-stack";
+import { RojAIAgentStack } from "../lib/rojai-agent-stack";
 
 const app = new cdk.App();
+
+// Existing listing generator stack
 new RojAIStack(app, "RojAIStack", {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
+// Autonomous merchandising agent stack (AWS Weekend Agent Challenge)
+new RojAIAgentStack(app, "RojAIAgentStack", {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
